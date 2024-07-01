4d29bb5b58293f6bfdbb791edcf9f85816ffe1f8
le tu <lethucthanhtu@gmail.com>
2024-07-01 21:55:10 +0700

    lethucthanhtu: fix color + add navigate when order placed successfully

--- app/src/main/java/com/prm392/estoreprm392/activity/order/OrderSuccessActivity.java
+++ app/src/main/java/com/prm392/estoreprm392/activity/order/OrderSuccessActivity.java
@@ -1,9 +1,14 @@
 package com.prm392.estoreprm392.activity.order;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.prm392.estoreprm392.R;
+import com.prm392.estoreprm392.activity.product.NewArrivalsActivity;
 
 public class OrderSuccessActivity extends AppCompatActivity {
 
@@ -11,5 +16,15 @@ public class OrderSuccessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_success);
+
+        Button btnGoHome = findViewById(R.id.btnGoHome);
+        btnGoHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OrderSuccessActivity.this, NewArrivalsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
